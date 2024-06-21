221b3f23605a2d7b43d3b861285f1f77854212cc
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-21 14:23:59 +0530

    store products in database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import Connnection from "./database/db.js";
+import DefaultData from './default.js'
 
 const app = express();
 
@@ -11,3 +12,5 @@ const PORT = 8000;
 
 Connnection();
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} hell`));
+
+DefaultData()
