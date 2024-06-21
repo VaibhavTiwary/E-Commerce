e46107a8d837a55bd7b8214315a068a527b71c75
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-21 09:13:55 +0530

    create carousel

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,9 +1,25 @@
 
+// import { Fragment } from "react";
 import Navbar from "./Navbar";
+import Banner from "./Banner";
+import { styled, Box } from "@mui/material"
+
+const Component = styled(Box)`
+    padding: 10px;
+    background: #F2F2F2;
+`
 
 const Home = () => {
     return (
-        <Navbar />
+        //It will show warning if we will not use div or fragments because jsx cannot return two parent components
+        //and now they are child of div.
+        //fragments are faster than div.
+        <>
+            <Navbar />
+            <Component>
+                <Banner />
+            </Component>
+        </>
     )
 }
 export default Home;
