dd74b2eecd1cfa8097040a7319f91009aea8d910
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-26 14:59:09 +0530

    mui grid setup for making ad images responsive

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Banner from "./Banner";
 import Slide from "./Slide";
 import MidSlide from "./MidSlide";
+import MidSection from "./MidSection";
 import { styled, Box } from "@mui/material"
 import { getProducts } from "../../Redux/actions/productActions";
 import { useDispatch, useSelector } from "react-redux";
@@ -31,6 +32,7 @@ const Home = () => {
             <Component>
                 <Banner />
                 <MidSlide products={products} title="Deal of the Day" timer={true} />
+                <MidSection />
                 <Slide products={products} title="Discounts for You" timer={false} />
                 <Slide products={products} title="Top Selection" timer={false} />
                 <Slide products={products} title="Trending Offers" timer={false} />
