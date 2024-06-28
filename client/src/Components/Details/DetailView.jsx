f2d0f066e453a701e239c3d271087cb2248057cf
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-28 11:02:16 +0530

    complete product detail page

--- client/src/Components/Details/DetailView.jsx
+++ client/src/Components/Details/DetailView.jsx
@@ -5,10 +5,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { getProductDetails } from "../../Redux/actions/productActions";
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Grid, styled } from '@mui/material';
+import ActionItem from "./ActionItem";
+import ProductDetail from "./ProductDetail";
+
+const Component = styled(Box)`
+    background: #F2F2F2;
+    margin-top: 55px
+`;
+
+const Container = styled(Grid)(({ theme }) => ({
+    background: '#FFFFFF',
+    display: 'flex',
+    [theme.breakpoints.down('md')]: {
+        margin: 0
+    }
+}))
+
+
+const RightContainer = styled(Grid)`
+    margin-top: 50px;
+    & > p {
+        margin-top: 10px;
+    }
+
+`
 
 const DetailView = () => {
-    const { loading, product } = useSelector(state => state.getProductDetails)
+    const { loading, product } = useSelector(state => state.getProductDetails);
+
     const dispatch = useDispatch();
     const { id } = useParams();
 
@@ -16,21 +41,24 @@ const DetailView = () => {
         if (product && id !== product.id)
             dispatch(getProductDetails(id))
     }, [dispatch, id, product, loading])
-    return (
-        <Box>
-            {
-                loading && product && Object.keys(product).length &&
-                <Box>
-                    <Box>
 
-                    </Box>
-                    <Box>
-                        <Typography>{product.title.longtitle}</Typography>
-                    </Box>
-                </Box>
+    return (
+        <Component>
+            {
+                product && Object.keys(product).length &&
+                <Container container>
+                    <Grid item lg={4} md={4} sm={8} xs={12}>
+                        <ActionItem product={product} />
+                    </Grid>
+                    <RightContainer item lg={8} md={8} sm={8} xs={12}>
+
+                        <ProductDetail product={product} />
+
+                    </RightContainer>
+                </Container>
 
             }
-        </Box>
+        </Component>
 
     )
 }
