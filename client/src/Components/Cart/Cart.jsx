8c88432b11b53c6654aff06c7681843b2b27956a
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-29 10:37:56 +0530

    insert product to cart reducer on clicking add to cart button

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,6 +1,30 @@
-
+import { useSelector } from "react-redux";
+import { Typography, Grid, Box } from "@mui/material";
 
 const Cart = () => {
+    const { cartItems } = useSelector(state => state.cart);
+
+    return (
+        <>
+            {
+                cartItems ?
+                    <Grid container>
+                        <Grid item lg={9} md={9} sm={12} xs={12}>
+                            <Box>
+                                <Typography style={{ fontWeight: 600, fontSize: 18 }}>My Cart ({cartItems?.length})</Typography>
+                            </Box>
+
+                        </Grid>
+                        <Grid item lg={3} md={3} sm={12} xs={12}>
+
+                        </Grid>
+                    </Grid>
+                    : <div>Empty</div>
+            }
+
+
+        </>
+    )
 
 }
 export default Cart;
--- client/src/Components/Details/ActionItem.jsx
+++ client/src/Components/Details/ActionItem.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
+
 import { Box, Button, styled } from '@mui/material';
 import { ShoppingCart as Cart, FlashOn as Flash } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../Redux/actions/cartActions';
 
 const LeftContainer = styled(Box)(({ theme }) => ({
     minWidth: '40%',
@@ -27,12 +32,23 @@ const StyledButton = styled(Button)(({ theme }) => ({
 }));
 
 const ActionItem = ({ product }) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [quantity, setQuantity] = useState(1);
+
+    const { id } = product;
+
+    const addItemToCart = () => {
+        dispatch(addToCart(id, quantity));
+        navigate('/Cart');
+    }
     return (
         <LeftContainer>
             <Box style={{ padding: '15px 20px', border: '1px solid #fofofo' }}>
                 <Image src={product.detailUrl} alt='product' />
             </Box>
-            <StyledButton variant="contained" style={{ marginRight: 10, background: '#ff9f00' }}><Cart />Add to Cart</StyledButton>
+            <StyledButton variant="contained" onClick={() => addItemToCart()} style={{ marginRight: 10, background: '#ff9f00' }}><Cart />Add to Cart</StyledButton>
             <StyledButton variant="contained" style={{ background: '#fb541B' }}><Flash />Buy Now</StyledButton>
         </LeftContainer>
     )
